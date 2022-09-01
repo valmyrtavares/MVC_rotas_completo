51ae958f8b3dbddffacda65d95c9eb5f39110fe4
Valmyr Tavares <valmyrtavares71@gmail.com>
2022-09-01 10:10:13 -0300

    incluindo o .env

---
+++ app/Common/Environment.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Common;
+
+class Environment{
+    public static function load($dir){
+        if(!file_exists($dir. '/.env')){
+            return false;
+        }
+        $lines = file($dir. '/.env');
+
+        
+
+        foreach($lines as $line){
+            putenv(trim($line));  
+        }
+    }
+}
--- index.php
+++ index.php
@@ -1,27 +1,22 @@
 <?php
  require  __DIR__. '/vendor/autoload.php';
 
- //use \App\Controller\Pages\Sobre;
- //use \App\Controller\Pages\Home;
  use \App\Http\Router;
- // use \App\Http\Response;
  use \App\Utils\View;
-
-
-
- define('URL', 'http://localhost/mvc-pratico');
+ use \App\Common\Environment;
+ 
+Environment::load(__DIR__);
+define('URL',getenv('URL'));
 
  View::init([
     'URL' => URL
  ]);
  
- $obRouter = new Router(URL);
+  $obRouter = new Router(URL);
 
 include __DIR__. '/routes/pages.php';
 
-// echo '<pre>';
-// print_r($obRouter);
-// exit;
+
  $obRouter->run()->sendResponse();
 
 
